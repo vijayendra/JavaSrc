d5ee74dcc4c5ca88bba2b8facc5686bc396f9fae
Vijayendra Bapte <vijayendra.bapte@gmail.com>
2014-10-16 10:49:56 -0700

    added for each loop example

---
+++ ForEach.java
@@ -0,0 +1,13 @@
+class ForEach{
+    public static void main(String[] args){
+        // testing for each loop
+        int[] intarray = {1, 2, 3, 5, 7, 9};
+        for(String arg: args){
+            System.out.println(arg);
+        }
+        for(int i: intarray){
+            System.out.println(i);
+        }
+
+    }
+}
