8aba5dc311011693e00ad37547842c2198c3fd49
Vijayendra Bapte <vijayendra.bapte@gmail.com>
2014-11-12 13:55:43 -0800

    added flymake hoook

--- prj.el
+++ prj.el
@@ -1,4 +1,7 @@
 (custom-set-variables
+ '(jde-compile-option-command-line-args nil)
+ '(jde-compile-option-debug (quote ("all" (t t t))))
+ '(jde-build-classpath (quote ("/home/egnyte/src/target/classes")))
  '(jde-global-classpath (quote ("/home/egnyte/src/target/classes")))
  '(jde-sourcepath (quote ("/home/egnyte/src")))
  )
@@ -116,3 +119,47 @@
                        '("if (debug) {" n> "System.out.println(\"" (p "Enter debug message:") "\");" n> "}")
                        "d"
                        "Inserts a print debug message")
+
+(defun flymake-java-ecj-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'jde-ecj-create-temp-file))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    ;; Change your ecj.jar location here
+    (list "java" (list "-jar" "/home/egnyte/Development/ecj.jar" "-Xemacs" "-d" "/tmp"
+                       "-source" "1.6" "-target" "1.6" 
+                       "-proceedOnError"
+                       "-classpath"
+                       (jde-build-classpath jde-global-classpath) local-file))))
+ 
+(defun flymake-java-ecj-cleanup ()
+  "Cleanup after `flymake-java-ecj-init' -- delete temp file and dirs."
+  (flymake-safe-delete-file flymake-temp-source-file-name)
+  (when flymake-temp-source-file-name
+    (flymake-safe-delete-directory (file-name-directory flymake-temp-source-file-name))))
+
+(defun jde-ecj-create-temp-file (file-name prefix)
+  "Create the file FILE-NAME in a unique directory in the temp directory."
+  (file-truename (expand-file-name (file-name-nondirectory file-name)
+                                   (expand-file-name  (int-to-string (random)) (flymake-get-temp-dir)))))
+ 
+(push '(".+\\.java$" flymake-java-ecj-init flymake-java-ecj-cleanup) flymake-allowed-file-name-masks)
+ 
+(push '("\\(.*?\\):\\([0-9]+\\): error: \\(.*?\\)\n" 1 2 nil 2 3 (6 compilation-error-face)) compilation-error-regexp-alist)
+ 
+(push '("\\(.*?\\):\\([0-9]+\\): warning: \\(.*?\\)\n" 1 2 nil 1 3 (6 compilation-warning-face)) compilation-error-regexp-alist)
+
+;; Sets the basic indentation for Java source files
+;; to two spaces.
+(defun my-jde-mode-hook ()
+  "Hook for running java file..."
+  (message " Loading my-jde-mode-hook...")
+  (c-set-offset 'substatement-open 0)
+  (c-set-offset 'statement-case-open 0)
+  (c-set-offset 'case-label '+)
+ (wisent-java-default-setup)
+ (flymake-mode)
+)
+
+(add-hook 'jde-mode-hook 'my-jde-mode-hook)
